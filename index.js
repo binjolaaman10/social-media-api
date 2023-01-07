edf7a23f00486d5da2aa2a59202b632dd4969466
Aman Binjola <abinjola@sparkcognition.com>
2023-01-07 10:08:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+const authController = require('./controllers/auth')
+const commentController = require('./controllers/comment')
+const postController = require('./controllers/post')
+const userController = require('./controllers/user')
+const app = express()
+
+// connect db
+mongoose.connect(process.env.MONGO_URL, () => console.log('Db is connected successfully'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get("/", (req, res) => {
+    res.send("all working")
+});
+app.use('/api', authController)
+app.use('/api', userController)
+app.use('/post', postController)
+app.use('/comment', commentController)
+
+// connect server
+app.listen(process.env.PORT, () => console.log('Server is connected successfully'))
